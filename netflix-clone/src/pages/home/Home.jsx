ce98b5f53c2da1c82d08dd25723650fd6b550316
Inshira Karim <karim.inshira@gmail.com>
2023-10-27 16:34:27 +0530

    checkpoint-2 added featured page

--- netflix-clone/src/pages/home/Home.jsx
+++ netflix-clone/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
 
 function Home() {
   return (
     <div>
-    <Navbar />     
+    <Navbar />   
+    <Featured type = "movie" />  
     </div>
   )
 }
