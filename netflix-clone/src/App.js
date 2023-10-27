faf054c33c9753f39ca3757f7bf51666631c97b8
Inshira Karim <karim.inshira@gmail.com>
2023-10-27 14:34:19 +0530

    checkpoint-1 added navbar component inside the home page

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/home/Home.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+   
+      <Home />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Navbar from '../../components/navbar/Navbar'
+
+function Home() {
+  return (
+    <div>
+    <Navbar />     
+    </div>
+  )
+}
+
+export default Home
